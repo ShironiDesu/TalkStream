4f0e5b156d934ce535e7524ebe58b168a968dd98
ShironiDesu <alishkingmu@gmail.com>
2024-12-03 00:34:24 +0500

    create chat-header and burger menu

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,44 @@
-const ChannelIdPage = () => {
-  return <div>Channel Id Page</div>;
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return RedirectToSignIn;
+  }
+  const channel = await db.channel.findUnique({
+    where: {
+      id: (await params).channelId,
+    },
+  });
+  const member = await db.member.findFirst({
+    where: {
+      serverId: (await params).serverId,
+      profileId: profile.id,
+    },
+  });
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
