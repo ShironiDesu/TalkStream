11d1bde73d4eb203b299ca30a83421fc4b96256f
ShironiDesu <alishkingmu@gmail.com>
2024-12-04 15:08:24 +0500

    messages api and file upload modal

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,6 +7,7 @@ import { Input } from "../ui/input";
 import { Plus, Smile } from "lucide-react";
 import qs from "query-string";
 import axios from "axios";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +24,7 @@ export default function ChatInput({
   name,
   type,
 }: ChatInputProps) {
+  const { onOpen } = useModal();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -53,7 +55,7 @@ export default function ChatInput({
                 <div className="relative p-4 pb-6">
                   <button
                     typeof="button"
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,51 +1,87 @@
-"use client"
+"use client";
 
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
 
-
-import '@uploadthing/react/styles.css'
-import { X } from "lucide-react";
+import "@uploadthing/react/styles.css";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
-
+import { useState } from "react";
 
 interface FileUploadProps {
-    onChange:(url?:string)=>void;
-    value:string;
-    endpoint:'messageFile'|'serverImage'
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
 }
-export const FileUpload = ({onChange,value,endpoint}:FileUploadProps) =>{
-    const fileType = value?.split('.').pop()
-    if(value && fileType !== 'pdf') {
-        return (<div className="relative h-20 w-20" >
-            <Image
-            fill
-            src={value}
-            alt="Upload"
-            className="rounded-full"
-             sizes="80px"
-            />
-            <button onClick={()=>onChange('')} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button"><X className="h-4 w-4"/></button>
-        </div>)
-    }
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  const [fileName, setFileName] = useState<string | null>(null);
+
+  const custumFileType = fileName?.split(".").pop();
+  //   console.log(custumFileType);
+  if (custumFileType === "pdf") {
     return (
-        
-       <UploadDropzone endpoint={endpoint} onUploadBegin={(name) => {
-      
-        console.log("Uploading: ", name);
-      }} onUploadProgress={(res)=>{
-      
-        
-       }} onClientUploadComplete={(res)=>{
-       
-        
-        onChange(res?.[0].url)
-     
-      
-       }} onUploadError={(error:Error)=>{
-      console.log(error);
-      
-        alert('Maximum image size is 4mb')
-       }}/>
-    )
-}
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className=" ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1
+            rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+          sizes="80px"
+        />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onUploadBegin={(name) => {
+        // console.log("Uploading: ", name);
+      }}
+      onUploadProgress={(res) => {}}
+      onClientUploadComplete={(res) => {
+        // console.log(res);
+        const uploadedFile = res?.[0];
+        setFileName(uploadedFile?.name || null);
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        // console.log(error);
+
+        alert("Maximum image size is 4mb");
+      }}
+    />
+  );
+};
