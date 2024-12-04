247bd296e3bd584a0a29fe69f99ea58430dff283
ShironiDesu <alishkingmu@gmail.com>
2024-12-04 17:11:19 +0500

    create chat message component

---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,50 @@
+import { useSocket } from "@/components/providers/socket-prodvider";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import qs from "query-string";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: String;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+export const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+
+  const fetchMessages = async ({ pageParam = undefined }) => {
+    const url = qs.stringifyUrl(
+      {
+        //@ts-ignore
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+    const res = await fetch(url);
+    return res.json();
+  };
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      initialPageParam: undefined,
+      queryKey: [queryKey],
+      queryFn: fetchMessages,
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+      refetchInterval: isConnected ? false : 1000,
+    });
+  return {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  };
+};
